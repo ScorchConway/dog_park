e2928c30a7d7668b29139a8e5815ce79d79edab0
Scott Conner <m.scott.conner@gmail.com>
2017-05-25 12:37:52 -0500

    Add files via upload
    because I got unmerged file errors when I tried to commit through the command line.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  def home
+    # @dogs = Dog.all_the_dogs_that_are_checked_in
+  end
+
+  def about
+  end
+end
---
+++ app/helpers/dog_helper.rb
@@ -0,0 +1,7 @@
+module DogHelper
+
+  def dog_pic_for (u)
+    #change this to a user's image of their dog
+    "pluto_the_dog"
+  end
+end
